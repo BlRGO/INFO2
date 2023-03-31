8201856fb3374532d9b134bf816a4824d55e35db
ynnsctr <124909859+ynnsctr@users.noreply.github.com>
2023-03-31 19:52:25 +0200

    ajout de l'exercie 4 du TP8 sur GitHub

---
+++ TP8/EXO4/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "rock.h"
+
+int main()
+{
+    printf("c'est l'exercice 4 du TP8 ! \n");
+    return 0;
+}
---
+++ TP8/EXO4/rock.h
@@ -0,0 +1,6 @@
+#ifndef __rock_H_
+#define __rock_H_
+
+
+
+#endif
